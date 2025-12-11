662b7390fe6e4d6d935462c03d517ef300c4505a
Yilmaz <yilmaz.eyidogan@gmail.com>
2025-12-11 15:43:15 +0300

    added Pot reader circuit

--- freemat/resistor.m
+++ freemat/resistor.m
@@ -15,3 +15,20 @@ R1 = 22e3;
 R2 = 3e3;
 Rpar = R1*R2 / (R1+R2);
 printf('Rpar = %.1f ohm\n', Rpar);
+
+Vs = 10;
+Rpot = 10e3;
+Rmid = 5e3;
+Rx = [0:1e3:10e3];
+
+Rup = 1000e3;
+Rdown = 330e3;
+
+for i = 1:11
+    Redown = (Rx(i) * (Rup+Rdown)) / (Rx(i) + (Rup+Rdown));
+    Vout2 = Vs * Redown / (Rpot - Rx(i) + Redown);
+    Vout2 = Vout2 - 150e-9 * (Rpot - Redown);
+    printf('POT Vout = %.2f V\n', Vout2);
+end
+
+    
