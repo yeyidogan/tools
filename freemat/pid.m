4a9bb39347b4cfdd389b35a733695a45affd5772
Yilmaz <yilmaz.eyidogan@gmail.com>
2026-02-03 13:30:55 +0300

    process variable

--- freemat/pid.m
+++ freemat/pid.m
@@ -1,12 +1,12 @@
-Kp = 0.11;
-Ki = 1;
-Kd = 0.1;
+Kp = 2;
+Ki = 10;
+Kd = 0.01;
 
 dt = 0.01;
-endTime = 10;
+endTime = 10-dt;
 time = 0:dt:endTime;
 
-output = zeros(size(time));
+pv = zeros(size(time));
 %input = sin(time);
 input(1) = 0;
 j = 1;
@@ -21,29 +21,30 @@ for i = 2:1000
         end
     end
 end
-input(length(time)) = 1.1;
 integral = 0;
 error(1) = 0;
+pv(1) = 0;
 
 for i = 2:1000
-    error(i) = input(i) - output(i-1);
+    error(i) = input(i) - pv(i-1);
     integral = integral + (error(i) * dt);
     derivative = (error(i) - error(i-1)) / dt;
-    if derivative > 10
-        derivative = 10;
-    end
-    if derivative < -10
-        derivative = -10;
-    end
-    output(i) = (Kp * error(i)) + (Ki * integral) + (Kd * derivative);
+    %if derivative > 10
+    %    derivative = 10;
+    %end
+    %if derivative < -10
+    %    derivative = -10;
+    %end
+    control(i) = (Kp * error(i)) + (Ki * integral) + (Kd * derivative);
+    pv(i) = pv(i-1) + (control(i) * dt);
     if rem(i, 100) == 0
-        printf('I=%0.2f; D=%0.2f; E=%.2f;\n', integral, derivative, error(i));
+        printf('I=%0.2f; D=%0.2f; E=%.2f; Pv=%0.2f\n', integral, derivative, error(i), pv(i));
     end
-    if (output(i) > 400) || (output(i) < -400)
-        break;
-    end
+    %if (control(i) > 400) || (control(i) < -400)
+    %    break;
+    %end
 end
 
 %plot(time, input);
-plot(time, input, time, output);
+plot(time, input, time, pv, time, control);
 %plot(error);
