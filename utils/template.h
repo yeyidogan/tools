44cae81a6d1133e5ed2076f370d247f52e75e0df
Yilmaz <yilmaz.eyidogan@gmail.com>
2026-04-23 07:32:36 +0300

    template.h

--- utils/template.h
+++ utils/template.h
@@ -3,10 +3,18 @@
  *      template.h, template functions
  *      Created date: 2026.03.27
  *******************************************************************************/
+#ifndef _TEMPLATE_H_
+#define _TEMPLATE_H_
 
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+
 /* Defines -------------------------------------------------------------------*/
+
+/* Structures ----------------------------------------------------------------*/
 
 /* Variables -----------------------------------------------------------------*/
 extern uint32_t u32;
@@ -14,4 +22,5 @@ extern uint32_t u32;
 /* Functions -----------------------------------------------------------------*/
 extern void func (void);
 
+#endif /* #ifndef _TEMPLATE_H_ */
 /* * * END OF FILE * * */
